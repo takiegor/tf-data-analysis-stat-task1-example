4b273431ed9cc09e553c9e97ffc548f106262f98
takiegor <95751108+takiegor@users.noreply.github.com>
2023-04-07 21:41:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,5 @@ import numpy as np
 chat_id = 451121038 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    a = x/10
-    a = np.add(a, np.random.exponential(1, x.shape[0]) - 39)
-    return a.mean() 
+
+    return np.add(x/10, np.random.exponential(1, x.shape[0]) - 39)
