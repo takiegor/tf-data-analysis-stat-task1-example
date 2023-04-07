e9e52c6009d7bf71d3c2e1051833c971851adcde
takiegor <95751108+takiegor@users.noreply.github.com>
2023-04-07 21:23:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,6 @@ import numpy as np
 chat_id = 451121038 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Рассчитать среднюю скорость
-    v_avg = np.mean(x)
-    а = v_avg / 10
-    return a.mean() # Ваш ответ
+    a = x/10
+    a = np.add(a, np.random.exponential(1, x.shape[0]) - 39)
+    return a.mean() 
